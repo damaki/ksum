59f4e09cb50d421c3b9432488b2a783bc9dc775f
Daniel King <damaki.gh@gmail.com>
2019-08-11 19:01:01 +0100

    Add tests to CI

--- travis_build.sh
+++ travis_build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 git clone https://github.com/damaki/libkeccak.git
-docker run -v $PWD:/app -w /app jklmnn/gnat:gpl.2019.spark /bin/sh -c "cd libkeccak && make install && cd .. && gprbuild -p -P ksum.gpr -f -j0 -XLIBKECCAK_BUILD=default"
+docker run -v $PWD:/app -w /app jklmnn/gnat:gpl.2019.spark /bin/sh -c "cd libkeccak && make install && cd .. && gprbuild -p -P ksum.gpr -f -j0 -XLIBKECCAK_BUILD=default && python3 tests/run_tests.py"
