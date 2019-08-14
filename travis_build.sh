67aae2543ebae81be90e0db9476ec6d699db25b6
Daniel King <damaki.gh@gmail.com>
2019-08-14 19:49:42 +0100

    Generate version.ads before invoking the Docker env

--- travis_build.sh
+++ travis_build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-docker run -v $PWD:/app -w /app jklmnn/gnat:gpl.2019.spark /bin/sh -c "cd libkeccak && make install && cd .. && sh build.sh"
+sh gen-version-ads.sh
+docker run -v $PWD:/app -w /app jklmnn/gnat:gpl.2019.spark /bin/sh -c "cd libkeccak && make install && cd .. && gprbuild -p -P ksum.gpr -j0 -XLIBKECCAK_BUILD=default"
 cd tests
 python3 run_tests.py --verbose
